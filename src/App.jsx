6023aa174375459f04965235342d520766efceb2
Sumit-source-dot <modisumit470@gmail.com>
2025-04-14 23:56:18 +0530

    Revert "resolved merge conflicts and merged sumit into main"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import './index.css'
 import HeroSection from './components/hero'
-import Testimonial23 from './components/clientReview'
 function App() {
   return (
     <div>
-      
      <HeroSection/>
-     <Testimonial23/>
     </div>
   )
 }
