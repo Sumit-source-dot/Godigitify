088fbaf9ad911af6dd5535097b15d2b994fb966d
Kapil <kapilguptakg1519@gmail.com>
2025-04-14 23:52:54 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './index.css'
 import HeroSection from './components/hero'
+import Navbar from './components/navbar'
+import Footer from './components/footer'
+import ChooseExpert from './components/chooseExpert'
 function App() {
   return (
     <div>
+     <Navbar/>
      <HeroSection/>
+     <ChooseExpert/>
+     <Footer/>    
     </div>
   )
 }
