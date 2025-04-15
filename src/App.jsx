a3421d05c930b0b38b635f6c0134a613ada8af07
Kapil <kapilguptakg1519@gmail.com>
2025-04-15 10:27:16 +0530

    resolved and merged kapil into main

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import './index.css'
 import HeroSection from './components/hero'
 import Testimonial23 from './components/clientReview'
+import Navbar from './components/navbar'
+import Footer from './components/footer'
+import ChooseExpert from './components/chooseExpert'
+import Faq from './components/faq'
+import About from './components/about'
 function App() {
   return (
     <div>
-      
+     <Navbar/>
      <HeroSection/>
+     <About/>
+     <ChooseExpert/>
      <Testimonial23/>
+     <Faq/>
+     <Footer/>
     </div>
   )
 }
